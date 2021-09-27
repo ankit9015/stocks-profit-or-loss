0a3c76ff530313ae8b492bd05dac981b7e1a7570
ankit9015 <67504902+ankit9015@users.noreply.github.com>
2021-09-28 01:43:34 +0530

    update: css edit

--- app.js
+++ app.js
@@ -8,10 +8,12 @@ function calculateProfitOrLoss(initial, quantity, current) {
     if(initial > current) {
         var loss = (initial - current) * quantity;
         var lossPercentage = (loss/(initial*quantity)) * 100;
+        outputMessage.style.color = "red";
         outputMessage.innerText = `You're makig a loss of Rs. ${loss} and the loss percentage is ${lossPercentage.toFixed(2)}%.`;
     } else if (initial < current) {
         var profit = (current - initial)*quantity;
         var profitPercentage = (profit/(current*quantity)) * 100;
+        outputMessage.style.color = "green";
         outputMessage.innerText = `You're makig a profit of Rs. ${profit} and the profit percentage is ${profitPercentage.toFixed(2)}%.`;
     } else {
         outputMessage.innerText = `You're neither making loss nor profit.`;
